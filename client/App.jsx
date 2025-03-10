3d627d28079867f6591b550e124736ca50cbc2d0
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-03-11 00:30:10 +0700

    feat: add mui

--- client/App.jsx
+++ client/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { routeTree } from "./routeTree";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
+const theme = createTheme({ colorSchemes: { dark: true } });
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider disableTransitionOnChange noSsr theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
