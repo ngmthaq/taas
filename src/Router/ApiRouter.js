54c4cc4572a7cb68e61ff2a9789a89b66d934b95
Nguyen Manh Thang <ngmthaq.20@gmail.com>
2025-05-07 20:33:22 +0700

    fix: routes name

--- src/Router/ApiRouter.js
+++ src/Router/ApiRouter.js
@@ -3,17 +3,17 @@ const apiRouter = express.Router();
 const SampleRequest = require("../Requests/SampleRequest");
 const SampleController = require("../Controllers/SampleController");
 
-const api = [
-  {
+const apiRoutes = {
+  sample: {
     method: "get",
     path: "/",
     middlewares: [SampleRequest.test],
     controller: SampleController.sampleMethod,
   },
-];
+};
 
-api.forEach((route) => {
+Object.values(apiRoutes).forEach((route) => {
   apiRouter[route.method](route.path, ...route.middlewares, route.controller);
 });
 
-module.exports = { apiRouter, api };
+module.exports = { apiRouter, apiRoutes };
--- src/Router/WebRouter.js
+++ src/Router/WebRouter.js
@@ -2,17 +2,17 @@ const express = require("express");
 const webRouter = express.Router();
 const HomeController = require("../Controllers/HomeController");
 
-const web = [
-  {
+const webRoutes = {
+  home: {
     method: "get",
     path: "/",
     middlewares: [],
     controller: HomeController.renderHomePage,
   },
-];
+};
 
-web.forEach((route) => {
+Object.values(webRoutes).forEach((route) => {
   webRouter[route.method](route.path, ...route.middlewares, route.controller);
 });
 
-module.exports = { webRouter, web };
+module.exports = { webRouter, webRoutes };
